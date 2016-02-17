d21da2aa4af7eacaa87462b1be1aee4449eb35d7
BramHendriksDeveloper <bram5688@gmail.com>
2016-02-17 12:22:26 +0100

    jquery mobile first steps

---
+++ www/js/data.js
@@ -0,0 +1,19 @@
+var data = {};
+
+data.films = [
+    { naam: "Nacho Libre", jaar: "2006", acteurs: [ { naam: "Jack Black", rol: "Nacho Libre" } ], rating: 5.6},
+    { naam: "Once Upon a Time in Mexico", jaar: "2020", acteurs: [ { naam: "Max Nijholt", rol: "Nacho Libre" }, { naam: "Bram Hendriks", rol: "El Bal" } ], rating: 9.4},
+    { naam: "Shrek 2", jaar: "2004", acteurs: [ { naam: "Antonio Banderas", rol: "Kat" }, { naam: "Donkey", rol: "Eddy Murphey" } ], rating: 7.2}
+];
+
+data.series = [
+    { naam: "Narcos", jaar: "2015", acteurs: [ { naam: "Wagner Moura", rol: "Pablo Escobar" }, { naam: "Pedro Pascal", rol: "Javier Peña" }], rating: 9.0},
+    { naam: "Breaking Pittig Slecht", jaar: "2008", acteurs: [ { naam: "Max Nijholt", rol: "Balto Black" }, { naam: "Bram Hendriks", rol: "Juan Roosmate" } ], rating: 9.5},
+    { naam: "Prison Kapot", jaar: "2005", acteurs: [ { naam: "Antonio Banderas", rol: "Kat" }, { naam: "Matthew McConaughey", rol: "MURPH" } ], rating: 3.7}
+];
+
+data.takenfilms = [
+    { naam: "Taken 1", jaar: "2008", acteurs: [ { naam: "Liam Nissan", rol: "Bryan Mills" } ], rating: 7.9},
+    { naam: "Taken 2", jaar: "2012", acteurs: [ { naam: "Liam Nissan", rol: "Brain Mollens" } ], rating: 6.3},
+    { naam: "Taken 3", jaar: "2014", acteurs: [ { naam: "Liam Toyota", rol: "Brain Milffz" } ], rating: 6.0}
+];
